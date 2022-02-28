2fce839ec1fb21cf73665c6eed49f6c2280db128
Muhammad Faizan <66197447+Muhammadfaizanjanjua109@users.noreply.github.com>
2022-02-28 23:28:05 +0500

    new changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <Router>
     <Navbar/>
       <Routes>
+        
      
         <Route exact path='/' element={<Home/>}/>
         <Route exact path='/About' element={<About/>}/> 
